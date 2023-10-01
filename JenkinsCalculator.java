ba9beca302b51672853918216ecffeb040158a67
“satya” <satya.ardhi786@gmail.com>
2023-10-01 20:35:20 +0530

    first commit

---
+++ JenkinsCalculator.java
@@ -0,0 +1,14 @@
+package com.satya.training.jenkins;
+
+public class JenkinsCalculator {
+
+	//Method to add 2 Numbers
+	public int addNumbers (int number1, int number2) {
+		return number1+number2;
+	}
+	
+	//Method to subtract 2 Numbers
+	public int subtractNumbers (int number1, int number2) {
+		return number1-number2;
+	}
+}
